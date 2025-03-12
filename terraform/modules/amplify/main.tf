6e5441835110e36ffaea5aefadbc116edfd14319
JulianLopez <julianmarcelolopez@gmail.com>
2025-03-12 15:55:06 -0300

    fixing amplify app rebuildings

--- terraform/modules/amplify/main.tf
+++ terraform/modules/amplify/main.tf
@@ -3,28 +3,19 @@ resource "aws_amplify_app" "frontend_app" {
   repository   = var.repository_url
   access_token = var.github_access_token
 
-  build_spec = var.build_spec != "" ? var.build_spec : <<-EOT
-version: 1
-frontend:
-  phases:
-    preBuild:
-      commands:
-        - 'npm ci --cache .npm --prefer-offline'
-    build:
-      commands:
-        - 'npm run build'
-  artifacts:
-    baseDirectory: dist
-    files:
-      - '**/*'
-  cache:
-    paths:
-      - '.npm/**/*'
-EOT
+  # Usa una variable separada para build_spec o un archivo externo para evitar recreaciones
+  build_spec = var.build_spec
 
-  environment_variables = var.environment_variables
+  # Usa lifecycle para prevenir reemplazos innecesarios
+  lifecycle {
+    ignore_changes = [
+      build_spec,
+      environment_variables
+    ]
+  }
 
-  platform = "WEB"
+  # Mantén estas configuraciones como variables para evitar cambios frecuentes
+  platform                   = "WEB"
   enable_auto_branch_creation = var.enable_auto_branch_creation
   enable_branch_auto_build    = var.enable_branch_auto_build
   enable_branch_auto_deletion = var.enable_branch_auto_deletion
@@ -40,6 +31,13 @@ resource "aws_amplify_branch" "frontend_branch" {
   enable_auto_build  = try(var.branches[var.branch_name].enable_auto_build, true)
 
   environment_variables = try(var.branches[var.branch_name].environment_variables, {})
+
+  # Añadir lifecycle para evitar recreaciones innecesarias
+  lifecycle {
+    ignore_changes = [
+      environment_variables
+    ]
+  }
 }
 
 # Ramas adicionales definidas en la variable branches
@@ -57,4 +55,11 @@ resource "aws_amplify_branch" "additional_branches" {
   enable_auto_build  = each.value.enable_auto_build
 
   environment_variables = each.value.environment_variables
+
+  # Añadir lifecycle para evitar recreaciones innecesarias
+  lifecycle {
+    ignore_changes = [
+      environment_variables
+    ]
+  }
 }
